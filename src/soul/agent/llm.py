8743b1fdb991bff0c7ee9f3255521545e29565a6
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-02 02:28:40 +0530

    implement llm handler

---
+++ src/soul/agent/llm.py
@@ -0,0 +1,17 @@
+"""
+LLMHandler will handle llm calling. 
+Initally we will only use local models using ollama.
+1. This will invoke request to ollama and handles.
+2. It will store messages history.
+3. It will return raw completion. 
+"""
+
+from dataclasses import dataclass 
+
+# TODO: implement LLMHandler
+class LLMHandler:
+    def __init__(self) -> None:
+    
+
+    def __call__(self) -> None:
+        pass
