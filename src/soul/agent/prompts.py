a23e41c8e06021eebd236fb96eeb31a56c5dc6fa
Harsh Bhatt <harshbhatt7585@gmail.com>
2026-03-05 18:41:54 +0530

    implement planning prompt

--- src/soul/agent/prompts.py
+++ src/soul/agent/prompts.py
@@ -41,7 +41,23 @@ def build_system_prompt(
 
 # TODO
 def build_planning_prompt(prompt: str):
-    pass
+    return (
+        "Think step by step and plan"
+        "1. What is the user's request?"
+        "2. What is the user's current context?"
+        "3. Identify do we need any tools for the user's request? If yes, identify the best tool to use to answer user's request?"
+        "4. Create a todo list to complete the user's request which inlcude steps to complete the user's request and the tool to use to complete the request."
+        "5. Return the response in JSON format."
+        
+        f"""For example: 
+        ```json
+        {
+            "plan": ...,
+            "tools": [tool_name_1, tool_name_2, ...]
+        }
+        ```
+        """
+    )
 
 # TODO
 def tool_identification_prompt(prompt: str):
